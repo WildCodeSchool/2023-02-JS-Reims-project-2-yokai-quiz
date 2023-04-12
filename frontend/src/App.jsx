d87486525a1cf132a7a1e417ee8849be7890a2d7
christophe Dubois <dubois51ch@gmail.com>
2023-04-12 12:05:22 +0200

    fetch front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Home from "./pages/Home";
 
 import "./App.css";
+
+const [templeData, setTempleData] = useState([]);
+
+const templeOfJapan = () => {
+  fetch("http://localhost:5000/temples")
+    .then((resp) => resp.json())
+    .then((data) => {
+      setTempleData(data);
+    });
+};
 
 function App() {
   return (
     <div className="App">
       <Home />
       <p>coucou</p>
+      <button type="button" onClick={templeOfJapan}>
+        click me
+      </button>
+      {templeData.map((temple) => (
+        <p key={temple.id}>{temple.name}</p>
+      ))}
     </div>
   );
 }
