816a50dfd1869e4914a823f36417caa929a53603
OkamiAka <nils.caqueux@gmail.com>
2023-04-13 15:28:19 +0200

    gestion requet

--- frontend/src/components/Quizz.jsx
+++ frontend/src/components/Quizz.jsx
@@ -5,9 +5,13 @@ let start = false;
 function Quizz() {
   const [quizz, setQuizz] = useState([]);
   const [numQuizz, setNumQuiz] = useState(0);
+
+  const amount = 5 + 5;
+  const difficulty = "easy";
+
   if (start === false) {
     fetch(
-      `https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=multiple`
+      `https://opentdb.com/api.php?amount=${amount}&category=31&difficulty=${difficulty}&type=multiple`
     )
       .then((resp) => resp.json())
       .then((data) => {
@@ -20,58 +24,55 @@ function Quizz() {
   };
   const answer = [];
   if (quizz.results) {
-    answer[0] = quizz.results[numQuizz].correct_answer;
-    answer[1] = quizz.results[numQuizz].incorrect_answers[0];
-    answer[2] = quizz.results[numQuizz].incorrect_answers[1];
-    answer[3] = quizz.results[numQuizz].incorrect_answers[2];
+    answer.push(quizz.results[numQuizz].correct_answer);
+    answer.push(quizz.results[numQuizz].incorrect_answers[0]);
+    answer.push(quizz.results[numQuizz].incorrect_answers[1]);
+    answer.push(quizz.results[numQuizz].incorrect_answers[2]);
   }
   return (
-    <>
-      <button type="button" onClick={addNumQuizz}>
-        getNumQuizz
-      </button>
-      {quizz.results && (
-        <>
-          <div>
-            <p>{quizz.results[numQuizz].category}</p>
-            <p>difficulty: {quizz.results[numQuizz].difficulty}</p>
-            <p>type: {quizz.results[numQuizz].type}</p>
-          </div>
-          <p>
+    quizz.results && (
+      <>
+        <div>
+          <p>{quizz.results[numQuizz].category}</p>
+          <p>difficulty: {quizz.results[numQuizz].difficulty}</p>
+          <p>type: {quizz.results[numQuizz].type}</p>
+        </div>
+        <p>
+          <h2>
             {quizz.results[numQuizz].question
               .replace(/&quot;/g, `"`)
               .replace(/&#039;/g, `'`)
               .replace(/&amp;/g, `&`)}
-          </p>
-          <p>
-            <button type="button" onClick={addNumQuizz}>
-              {answer[0]
-                .replace(/&quot;/g, `"`)
-                .replace(/&#039;/g, `'`)
-                .replace(/&amp;/g, `&`)}
-            </button>
-            <button type="button" onClick={addNumQuizz}>
-              {answer[1]
-                .replace(/&quot;/g, `"`)
-                .replace(/&#039;/g, `'`)
-                .replace(/&amp;/g, `&`)}
-            </button>
-            <button type="button" onClick={addNumQuizz}>
-              {answer[2]
-                .replace(/&quot;/g, `"`)
-                .replace(/&#039;/g, `'`)
-                .replace(/&amp;/g, `&`)}
-            </button>
-            <button type="button" onClick={addNumQuizz}>
-              {answer[3]
-                .replace(/&quot;/g, `"`)
-                .replace(/&#039;/g, `'`)
-                .replace(/&amp;/g, `&`)}
-            </button>
-          </p>
-        </>
-      )}
-    </>
+          </h2>
+        </p>
+        <p>
+          <button type="button" onClick={addNumQuizz}>
+            {answer[0]
+              .replace(/&quot;/g, `"`)
+              .replace(/&#039;/g, `'`)
+              .replace(/&amp;/g, `&`)}
+          </button>
+          <button type="button" onClick={addNumQuizz}>
+            {answer[1]
+              .replace(/&quot;/g, `"`)
+              .replace(/&#039;/g, `'`)
+              .replace(/&amp;/g, `&`)}
+          </button>
+          <button type="button" onClick={addNumQuizz}>
+            {answer[2]
+              .replace(/&quot;/g, `"`)
+              .replace(/&#039;/g, `'`)
+              .replace(/&amp;/g, `&`)}
+          </button>
+          <button type="button" onClick={addNumQuizz}>
+            {answer[3]
+              .replace(/&quot;/g, `"`)
+              .replace(/&#039;/g, `'`)
+              .replace(/&amp;/g, `&`)}
+          </button>
+        </p>
+      </>
+    )
   );
 }
 
