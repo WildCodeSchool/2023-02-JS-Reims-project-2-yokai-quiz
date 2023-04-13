c08c3bce178e635c1a4cd85358244dd4fc174f64
OkamiAka <nils.caqueux@gmail.com>
2023-04-13 15:05:01 +0200

    affichage des button

--- frontend/src/components/Quizz.jsx
+++ frontend/src/components/Quizz.jsx
@@ -18,19 +18,60 @@ function Quizz() {
   const addNumQuizz = () => {
     setNumQuiz(numQuizz + 1);
   };
+  const answer = [];
+  if (quizz.results) {
+    answer[0] = quizz.results[numQuizz].correct_answer;
+    answer[1] = quizz.results[numQuizz].incorrect_answers[0];
+    answer[2] = quizz.results[numQuizz].incorrect_answers[1];
+    answer[3] = quizz.results[numQuizz].incorrect_answers[2];
+  }
   return (
-    <div>
+    <>
       <button type="button" onClick={addNumQuizz}>
         getNumQuizz
       </button>
       {quizz.results && (
-        <p>
-          {quizz.results[numQuizz].question
-            .replace(/&quot;/g, `"`)
-            .replace(/&#039;/g, `'`)}
-        </p>
+        <>
+          <div>
+            <p>{quizz.results[numQuizz].category}</p>
+            <p>difficulty: {quizz.results[numQuizz].difficulty}</p>
+            <p>type: {quizz.results[numQuizz].type}</p>
+          </div>
+          <p>
+            {quizz.results[numQuizz].question
+              .replace(/&quot;/g, `"`)
+              .replace(/&#039;/g, `'`)
+              .replace(/&amp;/g, `&`)}
+          </p>
+          <p>
+            <button type="button" onClick={addNumQuizz}>
+              {answer[0]
+                .replace(/&quot;/g, `"`)
+                .replace(/&#039;/g, `'`)
+                .replace(/&amp;/g, `&`)}
+            </button>
+            <button type="button" onClick={addNumQuizz}>
+              {answer[1]
+                .replace(/&quot;/g, `"`)
+                .replace(/&#039;/g, `'`)
+                .replace(/&amp;/g, `&`)}
+            </button>
+            <button type="button" onClick={addNumQuizz}>
+              {answer[2]
+                .replace(/&quot;/g, `"`)
+                .replace(/&#039;/g, `'`)
+                .replace(/&amp;/g, `&`)}
+            </button>
+            <button type="button" onClick={addNumQuizz}>
+              {answer[3]
+                .replace(/&quot;/g, `"`)
+                .replace(/&#039;/g, `'`)
+                .replace(/&amp;/g, `&`)}
+            </button>
+          </p>
+        </>
       )}
-    </div>
+    </>
   );
 }
 
