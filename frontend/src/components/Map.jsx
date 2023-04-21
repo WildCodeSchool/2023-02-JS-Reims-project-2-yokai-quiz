2a26542ef155c70d36d429e7683460080006feb2
OkamiAka <nils.caqueux@gmail.com>
2023-04-21 15:48:56 +0200

    tooltip

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,20 +1,38 @@
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 import PropTypes from "prop-types";
 import templeImage from "../assets/Shinto.png";
 import map from "../assets/map_jap.png";
 
 function Map({ temples, setSwitchToTemple }) {
+  function click() {
+    setSwitchToTemple(true);
+  }
   return (
     <section className="mapContainer">
       <img src={map} alt="carte du japon" className="japonMap" />
       {temples.map((temple) => (
-        <button
-          onClick={() => setSwitchToTemple(true)}
-          type="button"
-          key={temple.id}
-          className={`buttonTemple buttonTemple${temple.id}`}
-        >
-          <img src={templeImage} alt={temple.templeName} />
-        </button>
+        <>
+          <button
+            onDoubleClick={click}
+            type="button"
+            key={temple.id}
+            className={`buttonTemple buttonTemple${temple.id}`}
+          >
+            <img src={templeImage} alt={temple.templeName} />
+          </button>
+          <Tooltip anchorSelect={`.buttonTemple${temple.id}`} clickable>
+            <h3>Temple {temple.templeName}</h3>
+            <p>Level: {temple.level}</p>
+            <p>
+              <img src="" alt={temple.yokaiName} />
+              {` ${temple.yokaiName}`}
+            </p>
+            <button type="button" onClick={click}>
+              Open Temple
+            </button>
+          </Tooltip>
+        </>
       ))}
     </section>
   );
