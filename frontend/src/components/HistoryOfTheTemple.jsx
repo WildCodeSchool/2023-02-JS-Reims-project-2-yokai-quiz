ef76fc90160b92d53bc6acf2d7522407ae49693a
OkamiAka <nils.caqueux@gmail.com>
2023-04-28 16:24:43 +0200

    Loading

--- frontend/src/components/HistoryOfTheTemple.jsx
+++ frontend/src/components/HistoryOfTheTemple.jsx
@@ -1,21 +1,29 @@
 import PropTypes from "prop-types";
+import loading from "../assets/loading.gif";
 
-function HistoryOfTheTemple({ setSwitchToQuizz, story }) {
+function HistoryOfTheTemple({ setSwitchToQuizz, story, checkLoading }) {
   return (
-    <div className="story">
-      <h3>Appearance: </h3>
-      <p>{story.appearance}</p>
-      <h3>Story: </h3>
-      <p>{story.origin}</p>
-      <button onClick={() => setSwitchToQuizz(true)} type="button">
-        skip⇒
-      </button>
-    </div>
+    <>
+      <div className="story">
+        <h3>Appearance: </h3>
+        <p>{story.appearance}</p>
+        <h3>Story: </h3>
+        <p>{story.origin}</p>
+      </div>
+      {checkLoading === true ? (
+        <button onClick={() => setSwitchToQuizz(true)} type="button">
+          Continue ⇒
+        </button>
+      ) : (
+        <img className="loading" src={loading} alt="loading" />
+      )}
+    </>
   );
 }
 
 HistoryOfTheTemple.propTypes = {
   setSwitchToQuizz: PropTypes.func.isRequired,
+  checkLoading: PropTypes.bool.isRequired,
   story: PropTypes.shape({
     appearance: PropTypes.string,
     origin: PropTypes.string,
