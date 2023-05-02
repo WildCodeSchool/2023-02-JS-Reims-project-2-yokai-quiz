3580c92953cae70ab3681001586f7776eda5df44
OkamiAka <nils.caqueux@gmail.com>
2023-05-02 14:01:18 +0200

    lacalStorage

--- frontend/src/components/Score.jsx
+++ frontend/src/components/Score.jsx
@@ -1,11 +1,34 @@
-import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 
-function Score({ score }) {
-  return <p>{score}</p>;
+function Score() {
+  const [templeData, setTempleData] = useState([]);
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/temples`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTempleData(data);
+      });
+  }, []);
+  // const templesData = localStorage.getItem("temples");
+  // const temples = JSON.parse(templesData);
+  // console.log(temples);
+  return (
+    <div className="score">
+      <p>{localStorage.getItem("playerName")}</p>
+      <div className="templeScore">
+        {templeData.map((temple) => (
+          <figure key={temple.id}>
+            <h3>{temple.templeName}</h3>
+            <img
+              src={`${import.meta.env.VITE_BACKEND_URL}${temple.yokaiImage}`}
+              alt={temple.yokaiName}
+            />
+            <figcaption>{temple.yokaiName}</figcaption>
+            <p>{temple.level}</p>
+          </figure>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-Score.propTypes = {
-  score: PropTypes.number.isRequired,
-};
-
 export default Score;
