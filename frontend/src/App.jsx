b1fea052ac3c20fb45eaad5c50d97604438bf008
OkamiAka <nils.caqueux@gmail.com>
2023-05-05 12:40:22 +0200

    corectino

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Game from "./components/Game";
 import Home from "./components/Home";
@@ -8,7 +8,7 @@ import "./App.scss";
 
 function App() {
   const [music, setMusic] = useState(localStorage.getItem("music"));
-  function musicHandle() {
+  const musicHandle = useCallback(() => {
     if (music === "true") {
       localStorage.setItem("music", "false");
       setMusic("false");
@@ -17,16 +17,13 @@ function App() {
       localStorage.setItem("music", "true");
       setMusic("true");
     }
-  }
+  }, [music, setMusic]);
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home musicHandle={musicHandle.bind} />} />
-        <Route path="/game" element={<Game musicHandle={musicHandle.bind} />} />
-        <Route
-          path="/score"
-          element={<Score musicHandle={musicHandle.bind} />}
-        />
+        <Route path="/" element={<Home musicHandle={musicHandle} />} />
+        <Route path="/game" element={<Game musicHandle={musicHandle} />} />
+        <Route path="/score" element={<Score musicHandle={musicHandle} />} />
       </Routes>
       {music === "true" && (
         <audio controls autoPlay>
